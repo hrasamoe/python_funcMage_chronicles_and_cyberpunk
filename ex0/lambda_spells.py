174fbe8a7aca3191cc28e827bbf4568138a05d8f
Heritiana <herysamuelpljv@gmail.com>
2026-07-07 13:00:09 +0300

    Chore: Function to get mages stats and transform spell names

--- ex0/lambda_spells.py
+++ ex0/lambda_spells.py
@@ -9,3 +9,19 @@ def power_filter(
     return list(filter(lambda x: x['power'] > min_power, mages))
 
 
+def spell_transformer(spells: list[str]) -> list[str]:
+    return list(map(lambda x: "* " + x + " *", spells))
+
+
+def mage_stats(mages: list[dict[str, any]]) -> dict[str, any]:
+    max_power = max(mages, key=lambda x: x['power'])
+    min_power = min(mages, key=lambda x: x['power'])
+    avg_power = round(sum(map(lambda x: x['power'], mages)) / len(mages), 2)
+
+    return {
+        'max_power': max_power['power'],
+        'min_power': min_power['power'],
+        'avg_power': avg_power
+    }
+
+
